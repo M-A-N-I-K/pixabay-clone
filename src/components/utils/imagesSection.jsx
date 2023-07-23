7da89dec6301284ff4e2d6c57b4bc4ba8c5aa9ac
Manik Dingra <manikdhingra0582@gmail.com>
2023-07-23 20:35:08 +0530

    Some files added in gitignore

--- src/components/utils/imagesSection.jsx
+++ src/components/utils/imagesSection.jsx
@@ -3,13 +3,15 @@ import ImageCard from './imageCard';
 
 const imagesSection = ({ images }) => {
     return (
-        <div className='w-[100vw] h-[55vh] mt-4 p-8 bg-white grid grid-cols-1 sm:gird-cols-2 md:grid-cols-3 items-end gap-6 overflow-y-visible z-25'>
-            {images?.map((image) => {
-                return (
-                    <ImageCard previewUrl={image.previewURL} largeImageUrl={image.largeImageURL} tags={image.tags} />
-                );
-            })}
-        </div>
+        <>
+            <div className='w-[100vw] h-[55vh] mt-4 p-8 bg-white grid grid-cols-1 sm:gird-cols-2 md:grid-cols-3 items-end gap-6 overflow-y-visible z-25'>
+                {images?.map((image) => {
+                    return (
+                        <ImageCard previewUrl={image.previewURL} largeImageUrl={image.largeImageURL} tags={image.tags} />
+                    );
+                })}
+            </div>
+        </>
     )
 }
 
